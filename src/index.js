c37230871b0082d39ed8539d72df02fcdd336c18
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-02-06 11:25:30 +0300

    add star rating component

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={25} color="red" />
+    <StarRating />
+  </React.StrictMode>
+);
